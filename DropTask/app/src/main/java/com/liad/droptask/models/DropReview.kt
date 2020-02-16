26509f619fa275fd86edd6cd056645f16ac9cac4
Liad Horovitz <liaddd@gmail.com>
2020-02-16 11:13:31 +0200

    removed unnecessary files.

--- DropTask/app/src/main/java/com/liad/droptask/models/DropReview.kt
+++
@@ -1,6 +0,0 @@
-package com.liad.droptask.models
-
-import androidx.room.Entity
-
-@Entity(tableName = "drops_table")
-data class DropReview (private val contact : Contact)
--- DropTask/app/src/main/java/com/liad/droptask/utils/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.liad.droptask.utils
-
-object Constants{
-
-    const val BASE_URL = "http://private-anon-2197580b74-droptask.apiary-proxy.com/"
-}
